527b0fea0d6c933cedcadd023674f7f3abce56cd
Quirax Lee <quiraxical@gmail.com>
2025-03-05 16:40:05 +0900

    Added advertising carousel

--- src/stories/MainPage.svelte
+++ src/stories/MainPage.svelte
@@ -1,6 +1,9 @@
 <svelte:options runes />
 
 <script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import * as Carousel from '$lib/components/ui/carousel';
+
 	import './MainPage.css';
 	import Header from './Header.svelte';
 
@@ -13,7 +16,28 @@
 		onLogin={() => (user = { name: 'Jane Doe' })}
 		onLogout={() => (user = undefined)}
 	/>
+
 	<!-- Advertising Carousel -->
+	<div class="relative flex justify-center px-17">
+		<Carousel.Root class="align-center aspect-video max-h-[30vh] max-w-full" opts={{ loop: true }}>
+			<Carousel.Previous />
+			<Carousel.Content class="w-full">
+				{#each Array(5) as _, i (i)}
+					<Carousel.Item>
+						<div class="aspect-video p-1">
+							<Card.Root class="aspect-video">
+								<Card.Content class="flex aspect-video items-center justify-center p-6">
+									<span class="text-4xl font-semibold">소개 또는 광고 이미지 {i + 1}</span>
+								</Card.Content>
+							</Card.Root>
+						</div>
+					</Carousel.Item>
+				{/each}
+			</Carousel.Content>
+			<Carousel.Next />
+		</Carousel.Root>
+	</div>
+
 	<!-- Card list of opened commission types -->
 	<!-- Card list of opened requests -->
 </article>
