bad0c28ab7ffa58d75eef289ed1af8eaede1ce7b
Quirax Lee <quiraxical@gmail.com>
2025-03-05 17:18:14 +0900

    Added tabs to distinguish user mode(requester, commisioner)

--- src/stories/MainPage.svelte
+++ src/stories/MainPage.svelte
@@ -3,12 +3,14 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
 	import * as Carousel from '$lib/components/ui/carousel';
+	import * as Tabs from '$lib/components/ui/tabs';
 	import { Input } from '$lib/components/ui/input';
 
 	import './MainPage.css';
 	import Header from './Header.svelte';
 
 	let user = $state<{ name: string }>();
+	let userMode = $state<string>('requester');
 </script>
 
 <article>
@@ -22,14 +24,43 @@
 		<h1 class="text-center text-3xl font-bold tracking-tight break-keep">
 			뭐하지공방에 오신 것을 환영합니다
 		</h1>
+
+		<Tabs.Root bind:value={userMode} class="w-[400px]">
+			<Tabs.List class="grid w-full grid-cols-2">
+				<Tabs.Trigger value="requester">저는 의뢰주입니다</Tabs.Trigger>
+				<Tabs.Trigger value="comissioner">저는 커미션주입니다</Tabs.Trigger>
+			</Tabs.List>
+		</Tabs.Root>
+
+		<!-- placeholder="커미션 타입 또는 의뢰 찾기" -->
 		<Input
 			type="search"
-			placeholder="커미션 타입 또는 의뢰 찾기"
+			placeholder={userMode === 'requester' ? '커미션 타입 찾기' : '의뢰 찾기'}
 			class="h-xl text-xl sm:w-full md:w-md"
 		/>
 	</section>
 
-	<section id="introducing" class="relative flex justify-center px-17">
+	<section id="recently-added" class="m-10">
+		<h2 class="text-2xl font-bold tracking-tight break-keep">
+			{#if userMode === 'requester'}
+				새로 개장한 커미션 타입들입니다
+			{:else}
+				새로 지원을 기다리는 의뢰들입니다
+			{/if}
+		</h2>
+		<!-- Card list of opened commission types -->
+	</section>
+
+	{#if userMode === 'requester'}
+		<section id="suggestion" class="m-10">
+			<h2 class="text-2xl font-bold tracking-tight break-keep">
+				원하는 커미션 타입을 찾지 못하셨나요?
+			</h2>
+			<!-- Card list of opened commission types -->
+		</section>
+	{/if}
+
+	<section id="introducing" class="relative my-10 flex justify-center px-17">
 		<Carousel.Root class="align-center aspect-video max-h-[30vh] max-w-full" opts={{ loop: true }}>
 			<Carousel.Previous />
 			<Carousel.Content class="w-full">
@@ -51,7 +82,4 @@
 			<Carousel.Next />
 		</Carousel.Root>
 	</section>
-
-	<!-- Card list of opened commission types -->
-	<!-- Card list of opened requests -->
 </article>
