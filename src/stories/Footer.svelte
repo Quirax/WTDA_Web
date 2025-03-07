4f62dffc2a4764a31182b44022be9f08e2c1a964
Quirax Lee <quiraxical@gmail.com>
2025-03-07 11:14:15 +0900

    Added contents to footer

--- src/stories/Footer.svelte
+++ src/stories/Footer.svelte
@@ -7,6 +7,17 @@
 	import logo from './assets/logo.png';
 </script>
 
-<footer class="bg-[hsl(var(--muted))] p-10 text-[hsl(var(--muted-foreground))]">
-	<P>뭐하지공방</P>
+<footer class="bg-[hsl(var(--muted))] p-10 text-sm text-[hsl(var(--muted-foreground))]">
+	<P class="text-base"
+		>공지사항 고객센터 이용약관 개인정보처리방침 사업자정보확인 트위터 마스토돈 블루스카이</P
+	>
+	<P
+		>상호: 뭐하지공방 | 대표: 홍길동 | 개인정보관리책임자: 홍길동 | 사업자등록번호: 123-45-67890 |
+		통신판매업신고: 2025-경기남양주-1234<br />주소: 경기도 남양주시 호평로68번길 21 | 이메일:
+		support@whattodo.at | 전화: 031-1234-5678</P
+	>
+	<P
+		>뭐하지공방은 통신판매중개자이며, 통신판매의 당사자가 아닙니다. 상품, 상품정보, 거래에 관한
+		의무와 책임은 계약 당사자 각자에게 있습니다.</P
+	>
 </footer>
