17f4b5b4bbc572a29d071fd26395ad736f890a0d
Quirax Lee <quiraxical@gmail.com>
2025-03-07 16:14:24 +0900

    Removed and added links

--- src/stories/Footer.svelte
+++ src/stories/Footer.svelte
@@ -24,7 +24,6 @@
 		<a href="." {...anchorProps}>고객센터</a>
 		<a href="." {...anchorProps}>이용약관</a>
 		<a href="." {...anchorProps}>개인정보처리방침</a>
-		<a href="." {...anchorProps}>사업자정보확인</a>
 		<a href="." {...anchorProps}>
 			<SIIcon icon={siX} {...iconProps} title="X(구 트위터) 공식 계정" />
 		</a>
@@ -39,10 +38,14 @@
 		<span>상호: 뭐하지공방</span>
 		<span>대표: 홍길동</span>
 		<span>개인정보관리책임자: 홍길동</span>
-		<span>사업자등록번호: 123-45-67890</span>
+		<span>
+			사업자등록번호: <a href="." {...anchorProps}>123-45-67890</a>
+		</span>
 		<span>통신판매업신고: 2025-경기남양주-1234</span>
 		<span>주소: 경기도 남양주시 호평로68번길 21</span>
-		<span>이메일: support@whattodo.at</span>
+		<span>
+			이메일: <a href="." {...anchorProps}>support@whattodo.at</a>
+		</span>
 		<span>전화: 031-1234-5678</span>
 	</P>
 	<P>
