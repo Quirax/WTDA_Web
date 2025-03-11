1ac7fab71f4f0252a48048d1c1eec018cb96984e
Quirax Lee <quiraxical@gmail.com>
2025-03-12 00:47:23 +0900

    Errata: fixed app.html and route files to serve Main Page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-	import 'svelte-material-ui/bare.css';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<!-- Material Icons -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-	<!-- Roboto -->
-	<link
-		rel="stylesheet"
-		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
-	/>
-	<!-- Roboto Mono -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono" />
-</svelte:head>
 
 <ParaglideJS {i18n}>
 	{@render children()}
