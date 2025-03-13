95bab0f8defa551111a32f461a8cb38f760d262b
Quirax Lee <quiraxical@gmail.com>
2025-03-13 13:52:54 +0900

    Changed border width and color of dropdown menu

--- src/lib/components/ui/dropdown-menu/dropdown-menu-content.svelte
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-content.svelte
@@ -1,27 +1,22 @@
 <script lang="ts">
-	import { DropdownMenu as DropdownMenuPrimitive } from "bits-ui";
-	import { cn, flyAndScale } from "$lib/utils.js";
+	import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils.js';
 
 	type $$Props = DropdownMenuPrimitive.ContentProps;
 	type $$Events = DropdownMenuPrimitive.ContentEvents;
 
-	let className: $$Props["class"] = undefined;
-	export let sideOffset: $$Props["sideOffset"] = 4;
-	export let transition: $$Props["transition"] = flyAndScale;
-	export let transitionConfig: $$Props["transitionConfig"] = undefined;
+	let className: $$Props['class'] = undefined;
+	export let sideOffset: $$Props['sideOffset'] = 4;
 	export { className as class };
 </script>
 
 <DropdownMenuPrimitive.Content
-	{transition}
-	{transitionConfig}
 	{sideOffset}
 	class={cn(
-		"bg-popover text-popover-foreground z-50 min-w-[8rem] rounded-md border p-1 shadow-md focus:outline-none",
-		className
+		'bg-popover text-popover-foreground z-50 min-w-[8rem] border-2 p-1 shadow-md focus:outline-none',
+		className,
 	)}
 	{...$$restProps}
-	on:keydown
->
+	on:keydown>
 	<slot />
 </DropdownMenuPrimitive.Content>
