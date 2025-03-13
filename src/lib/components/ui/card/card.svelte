c9968d9a14acd80b7cbde54be7e04fe998f3f7e0
Quirax Lee <quiraxical@gmail.com>
2025-03-13 13:23:18 +0900

    Changed width and light color of border of cards

--- src/lib/components/ui/card/card.svelte
+++ src/lib/components/ui/card/card.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils.js";
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
 
 	type $$Props = HTMLAttributes<HTMLDivElement>;
 
-	let className: $$Props["class"] = undefined;
+	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
-<div
-	class={cn("bg-card text-card-foreground rounded-lg border shadow-sm", className)}
-	{...$$restProps}
->
+<div class={cn('bg-card text-card-foreground border-4', className)} {...$$restProps}>
 	<slot />
 </div>
