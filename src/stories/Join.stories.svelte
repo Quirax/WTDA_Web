fe207e99ad59935bbe8885a33432efdca626face
Quirax Lee <quiraxical@gmail.com>
2025-03-17 11:57:50 +0900

    Added Join page

---
+++ src/stories/Join.stories.svelte
@@ -0,0 +1,18 @@
+<svelte:options runes />
+
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Join from './Join.svelte';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'User Management/Join',
+		component: Join,
+		parameters: {
+			// More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+			layout: 'fullscreen',
+		},
+	});
+</script>
+
+<Story name="Default" />
---
+++ src/stories/Join.svelte
@@ -0,0 +1,22 @@
+<svelte:options runes />
+
+<script lang="ts">
+	import { fn } from '@storybook/test';
+
+	import Layout from './Layout.svelte';
+
+	interface Props {
+		onSubmit?: (data: App.User) => boolean;
+	}
+
+	const { onSubmit = fn() }: Props = $props();
+
+	let user: App.User = {
+		id: '',
+		username: '',
+		profileImage: null,
+		fallbackInitial: '',
+	};
+</script>
+
+<Layout title="회원가입">내용없음</Layout>
