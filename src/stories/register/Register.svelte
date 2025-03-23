37f82672de5c53cfe13515eca5886f6dc2fddca7
Quirax Lee <quiraxical@gmail.com>
2025-03-23 17:20:00 +0900

    Fixed errors after migration

--- src/stories/register/Register.svelte
+++ src/stories/register/Register.svelte
@@ -62,83 +62,97 @@
 	<Section>
 		<H2>회원가입</H2>
 		<form method="POST" use:enhance class="w-2/3" action="?/do">
-			<Form.Field {form} name="email" class="my-4 flex flex-col space-y-1">
-				<Form.Control let:attrs>
-					<Form.Label><Badge variant="destructive">필수</Badge> 이메일</Form.Label>
-					<Input {...attrs} bind:value={$formData.email} {...$constraints.email} />
+			<Form.Field {form} name="email" class="flex flex-col my-4 space-y-1">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label><Badge variant="destructive">필수</Badge> 이메일</Form.Label>
+						<Input {...props} bind:value={$formData.email} {...$constraints.email} />
+					{/snippet}
 				</Form.Control>
 				<Form.FieldErrors />
 			</Form.Field>
-			<Form.Field {form} name="password" class="mb-4 flex flex-col space-y-1">
-				<Form.Control let:attrs>
-					<Form.Label><Badge variant="destructive">필수</Badge> 비밀번호</Form.Label>
-					<Input
-						{...attrs}
-						type="password"
-						bind:value={$formData.password}
-						{...$constraints.password} />
+			<Form.Field {form} name="password" class="flex flex-col mb-4 space-y-1">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label><Badge variant="destructive">필수</Badge> 비밀번호</Form.Label>
+						<Input
+							{...props}
+							type="password"
+							bind:value={$formData.password}
+							{...$constraints.password} />
+					{/snippet}
 				</Form.Control>
 				<Form.Description>최소 8자 이상</Form.Description>
 				<Form.FieldErrors />
 			</Form.Field>
-			<Form.Field {form} name="passwordConfirm" class="mb-4 flex flex-col space-y-1">
-				<Form.Control let:attrs>
-					<Form.Label><Badge variant="destructive">필수</Badge> 비밀번호 확인</Form.Label>
-					<Input
-						{...attrs}
-						type="password"
-						bind:value={$formData.passwordConfirm}
-						{...$constraints.passwordConfirm} />
+			<Form.Field {form} name="passwordConfirm" class="flex flex-col mb-4 space-y-1">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label><Badge variant="destructive">필수</Badge> 비밀번호 확인</Form.Label>
+						<Input
+							{...props}
+							type="password"
+							bind:value={$formData.passwordConfirm}
+							{...$constraints.passwordConfirm} />
+					{/snippet}
 				</Form.Control>
 				<Form.FieldErrors />
 			</Form.Field>
-			<Form.Field {form} name="username" class="mb-4 flex flex-col space-y-1">
-				<Form.Control let:attrs>
-					<Form.Label><Badge variant="destructive">필수</Badge> 닉네임</Form.Label>
-					<Input {...attrs} bind:value={$formData.username} {...$constraints.username} />
+			<Form.Field {form} name="username" class="flex flex-col mb-4 space-y-1">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label><Badge variant="destructive">필수</Badge> 닉네임</Form.Label>
+						<Input {...props} bind:value={$formData.username} {...$constraints.username} />
+					{/snippet}
 				</Form.Control>
 				<Form.Description>최대 20자</Form.Description>
 				<Form.FieldErrors />
 			</Form.Field>
 			<div class="mb-4 border-2">
 				<Form.Field {form} name="agree_eula" class="p-4">
-					<div class="flex flex-row items-center space-y-0 space-x-3">
-						<Form.Control let:attrs>
-							<Checkbox {...attrs} bind:checked={$formData.agree_eula} />
-							<div class="space-y-1 leading-none">
-								<Form.Label>
-									<Badge variant="destructive">필수</Badge> 뭐하지공방의 이용약관에 동의합니다.
-								</Form.Label>
-							</div>
-							<!-- Checkbox 사용 시 별도의 hidden field 필요 -->
-							<!-- ref: https://stackoverflow.com/a/78404901 -->
-							<input name={attrs.name} value={$formData.agree_eula} hidden />
+					<div class="flex flex-row items-center space-x-3 space-y-0">
+						<Form.Control>
+							{#snippet children({ props })}
+								<Checkbox {...props} bind:checked={$formData.agree_eula} />
+								<div class="space-y-1 leading-none">
+									<Form.Label>
+										<Badge variant="destructive">필수</Badge> 뭐하지공방의 이용약관에 동의합니다.
+									</Form.Label>
+								</div>
+								<!-- Checkbox 사용 시 별도의 hidden field 필요 -->
+								<!-- ref: https://stackoverflow.com/a/78404901 -->
+								<input name={props.name} value={$formData.agree_eula} hidden />
+							{/snippet}
 						</Form.Control>
 					</div>
 					<Form.FieldErrors />
 				</Form.Field>
 				<Form.Field {form} name="agree_privacypolicy" class="p-4">
-					<div class="flex flex-row items-center space-y-0 space-x-3">
-						<Form.Control let:attrs>
-							<Checkbox {...attrs} bind:checked={$formData.agree_privacypolicy} />
-							<div class="space-y-1 leading-none">
-								<Form.Label>
-									<Badge variant="destructive">필수</Badge> 뭐하지공방의 개인정보처리방침에 동의합니다.
-								</Form.Label>
-							</div>
-							<input name={attrs.name} value={$formData.agree_privacypolicy} hidden />
+					<div class="flex flex-row items-center space-x-3 space-y-0">
+						<Form.Control>
+							{#snippet children({ props })}
+								<Checkbox {...props} bind:checked={$formData.agree_privacypolicy} />
+								<div class="space-y-1 leading-none">
+									<Form.Label>
+										<Badge variant="destructive">필수</Badge> 뭐하지공방의 개인정보처리방침에 동의합니다.
+									</Form.Label>
+								</div>
+								<input name={props.name} value={$formData.agree_privacypolicy} hidden />
+							{/snippet}
 						</Form.Control>
 					</div>
 					<Form.FieldErrors />
 				</Form.Field>
 				<Form.Field {form} name="agree_marketing" class="p-4">
-					<div class="flex flex-row items-center space-y-0 space-x-3">
-						<Form.Control let:attrs>
-							<Checkbox {...attrs} bind:checked={$formData.agree_marketing} />
-							<div class="space-y-1 leading-none">
-								<Form.Label>뭐하지공방에서 제공하는 마케팅 정보 알림을 받겠습니다.</Form.Label>
-							</div>
-							<input name={attrs.name} value={$formData.agree_marketing} hidden />
+					<div class="flex flex-row items-center space-x-3 space-y-0">
+						<Form.Control>
+							{#snippet children({ props })}
+								<Checkbox {...props} bind:checked={$formData.agree_marketing} />
+								<div class="space-y-1 leading-none">
+									<Form.Label>뭐하지공방에서 제공하는 마케팅 정보 알림을 받겠습니다.</Form.Label>
+								</div>
+								<input name={props.name} value={$formData.agree_marketing} hidden />
+							{/snippet}
 						</Form.Control>
 					</div>
 					<Form.FieldErrors />
