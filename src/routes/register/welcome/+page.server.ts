231169d119b3f62c1fc9cc21fd04d1e4990a28c4
Quirax Lee <quiraxical@gmail.com>
2025-03-23 21:02:39 +0900

    Invalidated session to lead user to login

--- src/routes/register/welcome/+page.server.ts
+++ src/routes/register/welcome/+page.server.ts
@@ -1,13 +1,18 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { UserStatus } from '../../../app';
+import * as auth from '$lib/server/auth';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	if (!locals.user) throw redirect(302, '/');
-	if (locals.user.status === UserStatus.REQUIRED_EMAIL_CONFIRM)
+export const load: PageServerLoad = async (event) => {
+	if (!event.locals.user) throw redirect(302, '/');
+	if (event.locals.user.status === UserStatus.REQUIRED_EMAIL_CONFIRM)
 		throw redirect(302, '/register/email-confirm');
 
+	// Invalidate session to lead user to login
+	auth.invalidateSession(event.locals.session!.id);
+	auth.deleteSessionTokenCookie(event);
+
 	return {
-		user: locals.user,
+		user: event.locals.user,
 	};
 };
