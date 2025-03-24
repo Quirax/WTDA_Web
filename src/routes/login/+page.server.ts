4180abd73200a28e75a5519266ca1ccd5cd25b72
Quirax Lee <quiraxical@gmail.com>
2025-03-24 12:29:16 +0900

    Added login feature

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,6 +3,11 @@ import type { Actions, PageServerLoad } from './$types';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { formSchema } from '$lib/schema/login';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
+import { verify } from '@node-rs/argon2';
+import * as auth from '$lib/server/auth';
+import { eq } from 'drizzle-orm';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (locals.user) throw redirect(302, '/');
@@ -14,6 +19,35 @@ export const load: PageServerLoad = async ({ locals }) => {
 
 export const actions: Actions = {
 	default: async (event) => {
-		return fail(404, { message: 'Not found matched user' });
+		const form = await superValidate(event.request, zod(formSchema));
+
+		if (!form.valid) {
+			return fail(400, { message: 'The form is not valid.', formData: form.data });
+		}
+
+		const { email, password } = form.data;
+
+		const results = await db.select().from(table.user).where(eq(table.user.email, email));
+
+		const existingUser = results.at(0);
+		if (!existingUser) {
+			return fail(404, { message: 'Not found matched user' });
+		}
+
+		const validPassword = await verify(existingUser.passwordHash, password, {
+			memoryCost: 19456,
+			timeCost: 2,
+			outputLen: 32,
+			parallelism: 1,
+		});
+		if (!validPassword) {
+			return fail(404, { message: 'Not found matched user' });
+		}
+
+		const sessionToken = auth.generateSessionToken();
+		const session = await auth.createSession(sessionToken, existingUser.id);
+		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+
+		return redirect(302, '/');
 	},
 };
