e06a747e5024d039f1c4be25febf9dd47aa8d240
Quirax Lee <quiraxical@gmail.com>
2025-03-26 13:28:23 +0900

    Added cropper ui

---
+++ src/lib/components/cropper/cropper.svelte
@@ -0,0 +1,14 @@
+<svelte:options runes />
+
+<script lang="ts">
+	import Cropper, { type CropperProps } from 'svelte-easy-crop';
+
+	const { aspect = 1 / 1, ...restProps }: Partial<Omit<CropperProps, 'crop' | 'zoom'>> = $props();
+
+	let crop = $state({ x: 0, y: 0 });
+	let zoom = $state(1);
+</script>
+
+<div class="relative w-full aspect-square">
+	<Cropper bind:crop bind:zoom {aspect} {...restProps} />
+</div>
---
+++ src/lib/components/cropper/index.ts
@@ -0,0 +1,5 @@
+import Cropper from './cropper.svelte';
+
+export default Cropper;
+
+export * from './util';
