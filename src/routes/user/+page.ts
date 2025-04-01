a4221ee8f110c0a930b0e3a4b2fdf90d32bbdff2
Quirax Lee <quiraxical@gmail.com>
2025-04-01 15:02:51 +0900

    `/user` 접속 시 메인 페이지로 redirect

--- src/routes/user/+page.ts
+++ src/routes/user/+page.ts
@@ -1,5 +1,6 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-	return {};
+	throw redirect(302, '/');
 }) satisfies PageLoad;
