7f3627e51c9090233f0381e850248ed55119345a
Quirax Lee <quiraxical@gmail.com>
2025-04-30 18:30:40 +0900

    검색 키워드 입력 UI 추가 및 form으로 전환

--- src/stories/search/Search.svelte
+++ src/stories/search/Search.svelte
@@ -6,7 +6,7 @@
 	import { ArticleTypeText, CategoryText } from '$lib/messages';
 	import * as Popover from '$lib/components/ui/popover';
 	import { cn, isDesktop } from '$lib/utils';
-	import { buttonVariants } from '$lib/components/ui/button/button.svelte';
+	import Button, { buttonVariants } from '$lib/components/ui/button/button.svelte';
 	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
 	import RangeCalendar from '$lib/components/ui/range-calendar/range-calendar.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
@@ -27,96 +27,105 @@
 
 <Section>
 	<H2>'{query}' 검색결과</H2>
-	<nav class="my-2 flex flex-wrap space-y-2 space-x-2 border pt-2 pl-2">
-		<Select.Root type="multiple" name="search_range">
-			<!-- bind:value -->
-			<Select.Trigger class="w-[10em]">
-				{'검색 범위'}<!-- ArticleTypeText[$formData.type]() -->
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Item value={'title'}>제목</Select.Item>
-				<Select.Item value={'content'}>내용</Select.Item>
-				<Select.Item value={'tag'}>태그</Select.Item>
-			</Select.Content>
-		</Select.Root>
+	<form method="GET" class="space-y-2 pt-2 pl-2" action="/search">
+		<div class="flex">
+			<Input
+				type="search"
+				name="query"
+				placeholder="검색하기"
+				class="h-xl w-full border-stone-200 bg-stone-50 text-xl text-stone-950" />
+			<Button type="submit">검색</Button>
+		</div>
+		<div class="my-2 flex flex-wrap space-y-2 space-x-2">
+			<Select.Root type="multiple" name="search_range">
+				<!-- bind:value -->
+				<Select.Trigger class="w-[10em]">
+					{'검색 범위'}<!-- ArticleTypeText[$formData.type]() -->
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value={'title'}>제목</Select.Item>
+					<Select.Item value={'content'}>내용</Select.Item>
+					<Select.Item value={'tag'}>태그</Select.Item>
+				</Select.Content>
+			</Select.Root>
 
-		<Select.Root type="single" name="type">
-			<!-- bind:value -->
-			<Select.Trigger class="w-[10em]">
-				{'타입'}<!-- ArticleTypeText[$formData.type]() -->
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Item value={''}>모두</Select.Item>
-				{#each Object.entries(ArticleTypeText) as [k, v]}
-					<Select.Item value={k}>
-						{v()}
-					</Select.Item>
-				{/each}
-			</Select.Content>
-		</Select.Root>
+			<Select.Root type="single" name="type">
+				<!-- bind:value -->
+				<Select.Trigger class="w-[10em]">
+					{'타입'}<!-- ArticleTypeText[$formData.type]() -->
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value={''}>모두</Select.Item>
+					{#each Object.entries(ArticleTypeText) as [k, v]}
+						<Select.Item value={k}>
+							{v()}
+						</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
 
-		<Select.Root type="single" name="category">
-			<!-- bind:value -->
-			<Select.Trigger class="w-[10em]">
-				{'카테고리'}<!-- CategoryText[$formData.category]() -->
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Item value={''}>모두</Select.Item>
-				{#each Object.entries(CategoryText) as [k, v]}
-					<Select.Item value={k}>
-						{v()}
-					</Select.Item>
-				{/each}
-			</Select.Content>
-		</Select.Root>
+			<Select.Root type="single" name="category">
+				<!-- bind:value -->
+				<Select.Trigger class="w-[10em]">
+					{'카테고리'}<!-- CategoryText[$formData.category]() -->
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value={''}>모두</Select.Item>
+					{#each Object.entries(CategoryText) as [k, v]}
+						<Select.Item value={k}>
+							{v()}
+						</Select.Item>
+					{/each}
+				</Select.Content>
+			</Select.Root>
 
-		<Popover.Root>
-			<Popover.Trigger
-				class={cn(
-					buttonVariants({
-						variant: 'outline',
-						class: 'w-[10em] justify-start text-left font-normal',
-					}),
-					'text-muted-foreground',
-				)}>
-				<!-- !value -> muted -->
-				금액 범위
-			</Popover.Trigger>
-			<Popover.Content class="flex w-auto flex-col space-y-2 p-2">
-				<div class="flex items-center">
-					<span class="flex-none">최소:&nbsp;</span>
-					<Input placeholder="금액" type="currency" />
-					<!-- bind:value disabled -->
-					<span class="flex-none">&nbsp;포인트</span>
-				</div>
-				<div class="flex items-center">
-					<span class="flex-none">최대:&nbsp;</span>
-					<Input placeholder="금액" type="currency" />
-					<!-- bind:value disabled -->
-					<span class="flex-none">&nbsp;포인트</span>
-				</div>
-				<div class="flex flex-row space-x-2">
-					<Checkbox id="date-negotiable" />
-					<!-- bind:checked -->
-					<div class="leading-none">
-						<label for="date-negotiable">협상 가능한 경우를 포함함</label>
+			<Popover.Root>
+				<Popover.Trigger
+					class={cn(
+						buttonVariants({
+							variant: 'outline',
+							class: 'w-[10em] justify-start text-left font-normal',
+						}),
+						'text-muted-foreground',
+					)}>
+					<!-- !value -> muted -->
+					금액 범위
+				</Popover.Trigger>
+				<Popover.Content class="flex w-auto flex-col space-y-2 p-2">
+					<div class="flex items-center">
+						<span class="flex-none">최소:&nbsp;</span>
+						<Input placeholder="금액" type="currency" />
+						<!-- bind:value disabled -->
+						<span class="flex-none">&nbsp;포인트</span>
 					</div>
-				</div>
-			</Popover.Content>
-		</Popover.Root>
+					<div class="flex items-center">
+						<span class="flex-none">최대:&nbsp;</span>
+						<Input placeholder="금액" type="currency" />
+						<!-- bind:value disabled -->
+						<span class="flex-none">&nbsp;포인트</span>
+					</div>
+					<div class="flex flex-row space-x-2">
+						<Checkbox id="date-negotiable" />
+						<!-- bind:checked -->
+						<div class="leading-none">
+							<label for="date-negotiable">협상 가능한 경우를 포함함</label>
+						</div>
+					</div>
+				</Popover.Content>
+			</Popover.Root>
 
-		<Popover.Root>
-			<Popover.Trigger
-				class={cn(
-					buttonVariants({
-						variant: 'outline',
-						class: 'w-[10em] justify-start text-left font-normal',
-					}),
-					'text-muted-foreground',
-				)}>
-				<!-- !value -> muted -->
-				일정 범위
-				<!-- {#if value && value.start}
+			<Popover.Root>
+				<Popover.Trigger
+					class={cn(
+						buttonVariants({
+							variant: 'outline',
+							class: 'w-[10em] justify-start text-left font-normal',
+						}),
+						'text-muted-foreground',
+					)}>
+					<!-- !value -> muted -->
+					일정 범위
+					<!-- {#if value && value.start}
         {#if value.end}
           {df.format(value.start.toDate(getLocalTimeZone()))} - {df.format(
             value.end.toDate(getLocalTimeZone())
@@ -129,47 +138,48 @@
       {:else}
         Pick a date
       {/if} -->
-				<!-- df.format(value.toDate(getLocalTimeZone())) -->
-			</Popover.Trigger>
-			<Popover.Content class="flex w-auto flex-col space-y-2 p-2">
-				<div class="border">
-					<RangeCalendar numberOfMonths={2} locale="ko-KR" />
-					<!-- bind:value onStartValueChange -->
-				</div>
-				<div class="flex flex-row space-x-2">
-					<Checkbox id="date-negotiable" />
-					<!-- bind:checked -->
-					<div class="leading-none">
-						<label for="date-negotiable">협상 가능한 경우를 포함함</label>
+					<!-- df.format(value.toDate(getLocalTimeZone())) -->
+				</Popover.Trigger>
+				<Popover.Content class="flex w-auto flex-col space-y-2 p-2">
+					<div class="border">
+						<RangeCalendar numberOfMonths={2} locale="ko-KR" />
+						<!-- bind:value onStartValueChange -->
 					</div>
-				</div>
-			</Popover.Content>
-		</Popover.Root>
+					<div class="flex flex-row space-x-2">
+						<Checkbox id="date-negotiable" />
+						<!-- bind:checked -->
+						<div class="leading-none">
+							<label for="date-negotiable">협상 가능한 경우를 포함함</label>
+						</div>
+					</div>
+				</Popover.Content>
+			</Popover.Root>
 
-		<Select.Root type="single" name="commercial_use">
-			<!-- bind:value -->
-			<Select.Trigger class="w-[10em]">
-				{'상업적 목적'}<!-- ArticleTypeText[$formData.type]() -->
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Item value={'all'}>모두</Select.Item>
-				<Select.Item value={'excluded'}>제외</Select.Item>
-				<Select.Item value={'required'}>필수</Select.Item>
-			</Select.Content>
-		</Select.Root>
+			<Select.Root type="single" name="commercial_use">
+				<!-- bind:value -->
+				<Select.Trigger class="w-[10em]">
+					{'상업적 목적'}<!-- ArticleTypeText[$formData.type]() -->
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value={'all'}>모두</Select.Item>
+					<Select.Item value={'excluded'}>제외</Select.Item>
+					<Select.Item value={'required'}>필수</Select.Item>
+				</Select.Content>
+			</Select.Root>
 
-		<Select.Root type="single" name="adult_contents">
-			<!-- bind:value -->
-			<Select.Trigger class="w-[10em]">
-				{'성인 콘텐츠'}<!-- ArticleTypeText[$formData.type]() -->
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Item value={'all'}>모두</Select.Item>
-				<Select.Item value={'excluded'}>제외</Select.Item>
-				<Select.Item value={'required'}>필수</Select.Item>
-			</Select.Content>
-		</Select.Root>
-	</nav>
+			<Select.Root type="single" name="adult_contents">
+				<!-- bind:value -->
+				<Select.Trigger class="w-[10em]">
+					{'성인 콘텐츠'}<!-- ArticleTypeText[$formData.type]() -->
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value={'all'}>모두</Select.Item>
+					<Select.Item value={'excluded'}>제외</Select.Item>
+					<Select.Item value={'required'}>필수</Select.Item>
+				</Select.Content>
+			</Select.Root>
+		</div>
+	</form>
 	<section>
 		<ArticleList
 			id="contents-list"
