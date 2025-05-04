bb230c628c601b30f80810745f91f7853cf13081
Quirax Lee <quiraxical@gmail.com>
2025-05-04 15:16:03 +0900

    paraglide compile 시 esbuild가 죽는 문제 해결을 위해 paraglide 최신화

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import { sequence } from '@sveltejs/kit/hooks';
-import { i18n } from '$lib/i18n';
 import type { Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
+import { paraglideMiddleware } from '$lib/paraglide/server';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
@@ -24,5 +24,14 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
-const handleParaglide: Handle = i18n.handle();
-export const handle: Handle = sequence(handleAuth, handleParaglide);
+const paraglideHandle: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+		event.request = localizedRequest;
+		return resolve(event, {
+			transformPageChunk: ({ html }) => {
+				return html.replace('%lang%', locale);
+			},
+		});
+	});
+
+export const handle: Handle = sequence(handleAuth, paraglideHandle);
