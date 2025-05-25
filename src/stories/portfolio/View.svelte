3e0f3f9ecb5360cc06a93c016ac54136fc841129
Quirax Lee <quiraxical@gmail.com>
2025-05-25 20:52:54 +0900

    슬라이드 쇼에 전체 미디어 목록 표시 및 현재 선택된 항목 표시

--- src/stories/portfolio/View.svelte
+++ src/stories/portfolio/View.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { CategoryText } from '$lib/messages';
-	import { formatDatetimeString, sanitizeHTML } from '$lib/utils';
+	import { cn, formatDatetimeString, sanitizeHTML } from '$lib/utils';
 	import Avatar from '$stories/components/Avatar.svelte';
 	import Header from '$stories/components/Header.svelte';
 	import Section from '$stories/components/Section.svelte';
@@ -16,6 +16,7 @@
 	import { page } from '$app/state';
 	import * as Card from '$lib/components/ui/card';
 	import * as Carousel from '$lib/components/ui/carousel';
+	import type { CarouselAPI } from '$lib/components/ui/carousel/context';
 
 	interface Props extends ReturnType<typeof $props> {
 		article: App.Portfolio;
@@ -45,6 +46,18 @@
 
 		openAfterDeletionAlert = true;
 	};
+
+	let api = $state<CarouselAPI>();
+	let current = $state(0);
+
+	$effect(() => {
+		if (api) {
+			current = api.selectedScrollSnap();
+			api.on('select', () => {
+				current = api!.selectedScrollSnap();
+			});
+		}
+	});
 </script>
 
 <svelte:head>
@@ -64,7 +77,10 @@
 		<!-- media -->
 		{#if article.media.length > 0}
 			<section class="relative my-4 flex justify-center px-17">
-				<Carousel.Root class="align-center h-[50vh] max-w-full" opts={{ loop: true }}>
+				<Carousel.Root
+					class="align-center h-[50vh] max-w-full"
+					opts={{ loop: true }}
+					setApi={(emblaApi) => (api = emblaApi)}>
 					<Carousel.Previous />
 					<Carousel.Content class="w-full">
 						{#each article.media as media, idx}
@@ -80,6 +96,24 @@
 					<Carousel.Next />
 				</Carousel.Root>
 			</section>
+			<div class="flex w-full justify-center">
+				<Carousel.Root class="align-center" opts={{ loop: true, align: 'start' }}>
+					<Carousel.Content class="w-44 md:w-88 lg:w-132 xl:w-176 2xl:w-220">
+						{#each article.media as media, idx}
+							<Carousel.Item
+								class="relative aspect-square h-40 md:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
+								<div class="size-full p-1">
+									<Card.Root class={cn('size-full', idx === current && 'border-primary border-3')}>
+										<img class="size-full object-cover" src={media} alt="첨부 미디어 {idx + 1}" />
+									</Card.Root>
+								</div>
+							</Carousel.Item>
+						{/each}
+					</Carousel.Content>
+					<Carousel.Previous />
+					<Carousel.Next />
+				</Carousel.Root>
+			</div>
 		{/if}
 		<article class="html p-4">
 			{#if article.content}
