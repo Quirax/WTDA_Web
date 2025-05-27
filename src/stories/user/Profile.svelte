b45f6456006176f6276543b4f9dcfa23ec928997
Quirax Lee <quiraxical@gmail.com>
2025-05-27 13:01:27 +0900

    사용자 작성 게시물 관련 코드 분리

--- src/stories/user/Profile.svelte
+++ src/stories/user/Profile.svelte
@@ -17,7 +17,6 @@
 		TriangleAlert,
 		NotepadTextDashed,
 		Trash2,
-		CirclePlus,
 	} from 'lucide-svelte';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import * as Alert from '$lib/components/ui/alert/index.js';
@@ -66,9 +65,8 @@
 	import tinycolor from 'tinycolor2';
 	import Cropper from '$lib/components/cropper/cropper.svelte';
 	import AlertDialog from '$stories/components/AlertDialog.svelte';
-	import ArticleList from '$stories/components/ArticleList.svelte';
-	import * as Tabs from '$lib/components/ui/tabs';
 	import { untrack } from 'svelte';
+	import UserArticles from './UserArticles.svelte';
 
 	interface Props extends ReturnType<typeof $props> {
 		user: Omit<NonNullable<App.User>, 'status'>;
@@ -393,47 +391,6 @@
 
 	// Profile Edit Mode
 	let profileEditMode = $state(false);
-
-	// Article List
-	let articleListTab = $state<'all' | 'requests'>('all');
-	let articleList = $state<App.Articles[] | undefined | null>();
-	let articlePage = $state(1);
-	let articleTotal = $state(0);
-
-	$effect(() => {
-		if (articleListTab) articlePage = 0;
-	});
-
-	$effect(() => {
-		if (articlePage === 0) {
-			articlePage = 1;
-			return;
-		}
-
-		(async () => {
-			const formData = new FormData();
-			formData.append(
-				'tab',
-				untrack(() => articleListTab),
-			);
-			formData.append('page', articlePage.toString());
-
-			// ref: https://svelte.dev/docs/kit/$app-forms#applyAction
-			const result = await fetch('?/articles', { method: 'post', body: formData })
-				.then((r) => r.text())
-				.then((r) => deserialize(r));
-
-			if (result.type === 'success') {
-				articleList = result.data?.list as App.Articles[];
-				articleTotal = (result.data?.count as number) || 0;
-			} else {
-				articleList = null;
-				articleTotal = 0;
-				articlePage = 1;
-				console.error(result);
-			}
-		})();
-	});
 
 	// Profile link copy
 	let openLinkCopyAlert = $state(false);
@@ -935,45 +892,7 @@
 				</div>
 			</div>
 		</section>
-		<section class="space-y-4">
-			<div class="flex justify-between">
-				<Tabs.Root bind:value={articleListTab} class="md:w-[400px]">
-					<Tabs.List class="[&>*]:text-lg [&>*]:font-bold">
-						<Tabs.Trigger value="all">전체</Tabs.Trigger>
-						<!-- <Tabs.Trigger value="commission_types">커미션 타입</Tabs.Trigger> -->
-						<Tabs.Trigger value="requests">의뢰</Tabs.Trigger>
-						<Tabs.Trigger value="portfolio">포트폴리오</Tabs.Trigger>
-					</Tabs.List>
-				</Tabs.Root>
-
-				<DropdownMenu.Root>
-					<DropdownMenu.Trigger class="m-0 p-0">
-						{#snippet child({ props })}
-							<Button {...props} variant="default" class="px-4"><CirclePlus /> 새로 만들기</Button>
-						{/snippet}
-					</DropdownMenu.Trigger>
-					<DropdownMenu.Content class="w-56" align="end">
-						<DropdownMenu.Group>
-							<DropdownMenu.Item onclick={() => goto('/r/create')}>의뢰</DropdownMenu.Item>
-							<DropdownMenu.Item onclick={() => goto('/pf/create')}>포트폴리오</DropdownMenu.Item>
-						</DropdownMenu.Group>
-					</DropdownMenu.Content>
-				</DropdownMenu.Root>
-			</div>
-
-			<ArticleList
-				class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
-				accentColor={user.profile.accentColor}
-				articles={articleList || []} />
-
-			{#if articleTotal > 0}
-				<Pagination
-					bind:page={articlePage}
-					count={articleTotal}
-					perPage={profileArticlesPerPage}
-					siblingCount={isDesktop() ? 1 : 0} />
-			{/if}
-		</section>
+		<UserArticles {user} />
 	</section>
 </main>
 
