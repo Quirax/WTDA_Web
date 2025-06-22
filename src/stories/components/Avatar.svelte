c83546c2f8d6fdb7f4f04e2f9be1f6e1064f4d1a
Quirax Lee <quiraxical@gmail.com>
2025-06-22 22:01:40 +0900

    기타: 아바타 클릭 시 해당 사용자 프로필로 이동

--- src/stories/components/Avatar.svelte
+++ src/stories/components/Avatar.svelte
@@ -6,19 +6,22 @@
 	import User from 'lucide-svelte/icons/user';
 
 	interface Props extends HTMLSlotAttributes {
-		user: Pick<NonNullable<App.User>, 'profileImage' | 'username'> | null;
+		user: Pick<NonNullable<App.User>, 'id' | 'profileImage' | 'username'> | null;
 	}
 
 	const {
 		class: className = undefined,
 		user = {
+			id: '',
 			username: '',
 			profileImage: null,
 		},
 	}: Props = $props();
 </script>
 
-<Avatar.Root class={className}>
-	<Avatar.Image src={user?.profileImage} alt={user?.username} />
-	<Avatar.Fallback><User /></Avatar.Fallback>
-</Avatar.Root>
+<a href="/user/{user?.id}">
+	<Avatar.Root class={className}>
+		<Avatar.Image src={user?.profileImage} alt={user?.username} />
+		<Avatar.Fallback><User /></Avatar.Fallback>
+	</Avatar.Root>
+</a>
