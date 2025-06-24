a383b699382fbec44fcf4c32d5fc095c5f8ab48f
Quirax Lee <quiraxical@gmail.com>
2025-06-24 23:36:38 +0900

    데스크톱 알림 권한 획득 과정 구현

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,11 @@
 	import { afterNavigate, invalidate } from '$app/navigation';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher } from 'mode-watcher';
+	import { onMount } from 'svelte';
+	import { toast } from 'svelte-sonner';
+	import { browser, dev } from '$app/environment';
+	import { getFirebaseApp, getMessaging, getToken } from '$lib/firebase';
+	import { env } from '$env/dynamic/public';
 
 	interface Props extends ReturnType<typeof $props> {
 		data: LayoutServerData;
@@ -22,6 +27,67 @@
 			sessionStore.set(data.session);
 		});
 	});
+
+	if (browser) {
+		const firebase = getFirebaseApp();
+		const messaging = getMessaging(firebase);
+
+		const requestNotificationPermission = (
+			reject?: (_: 'permission_rejected' | 'token_failed') => void,
+		) => {
+			Notification.requestPermission()
+				.then((permission) => {
+					if (permission === 'granted') {
+						// ref: https://svelte.dev/docs/kit/service-workers#During-development
+						navigator.serviceWorker
+							.register('/service-worker.js', {
+								type: dev ? 'module' : 'classic',
+							})
+							.then((register) =>
+								getToken(messaging, {
+									vapidKey: env.PUBLIC_FIREBASE_VAPID_KEY,
+									serviceWorkerRegistration: register,
+								}),
+							)
+							.then((token) => {
+								/* noop */
+							})
+							.catch((err) => {
+								console.error(err);
+								reject?.('token_failed');
+							});
+					} else {
+						reject?.('permission_rejected');
+					}
+				})
+				.catch(() => {
+					reject?.('permission_rejected');
+				});
+		};
+
+		onMount(() => {
+			if ('serviceWorker' in navigator) {
+				requestNotificationPermission((reason) => {
+					switch (reason) {
+						case 'permission_rejected':
+							toast.info('데스크톱 알림을 받으시려면 알림을 허용해주세요.', {
+								action: {
+									label: '알림 허용',
+									onClick: () => requestNotificationPermission,
+								},
+							});
+							break;
+						case 'token_failed':
+							toast.error('데스크톱 알림 활성화를 위한 토큰을 얻지 못했습니다.', {
+								description:
+									'Brave 브라우저를 사용하는 경우 설정에서 "푸시 메시지에 Google 서비스 사용"를 활성화해주세요.',
+							});
+							break;
+					}
+				});
+			}
+		});
+	}
 </script>
 
 <ModeWatcher />
