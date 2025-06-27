8bf70d2a113e9aa45e77733d9115adf8320c8524
Quirax Lee <quiraxical@gmail.com>
2025-06-27 15:52:20 +0900

    사이드 메뉴가 있는 경우의 레이아웃 템플릿 적용

--- src/stories/admin/Layout.svelte
+++ src/stories/admin/Layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+	import SidebarLayout from '$stories/components/SidebarLayout.svelte';
 	import type { Snippet } from 'svelte';
 
 	let { children }: { children: Snippet } = $props();
 </script>
 
-{@render children()}
+<SidebarLayout containerClass={undefined}>
+	{#snippet nav()}
+		나-브
+	{/snippet}
+	{#snippet content()}
+		{@render children()}
+	{/snippet}
+</SidebarLayout>
---
+++ src/stories/components/SidebarLayout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
+	import type { ClassValue } from 'tailwind-variants';
+
+	interface Props {
+		containerClass?: ClassValue;
+		navClass?: ClassValue;
+		contentClass?: ClassValue;
+		nav: Snippet;
+		content: Snippet;
+	}
+
+	const { containerClass, navClass, contentClass, nav, content }: Props = $props();
+</script>
+
+<div class={cn('w-ful flex max-md:flex-col', containerClass)}>
+	<nav
+		class={cn(
+			'bg-background mt-16 w-70 flex-none overflow-y-auto max-md:w-full',
+			!!page.params.id && 'max-md:hidden',
+			navClass,
+		)}>
+		{@render nav()}
+	</nav>
+
+	<div class={cn('size-full', contentClass)}>
+		{@render content()}
+	</div>
+</div>
