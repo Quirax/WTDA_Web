284e1f36f1ce55e3ae5eda416e73aea6fb3be9fb
Quirax Lee <quiraxical@gmail.com>
2025-07-01 12:09:04 +0900

    관리자 페이지 접근 시 비밀번호 확인을 거치도록 강제

---
+++ src/routes/admin/auth/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import PasswordAuth from '$stories/login/PasswordAuth.svelte';
+	import type { PageServerData } from './$types';
+
+	let { data }: { data: PageServerData } = $props();
+</script>
+
+<PasswordAuth data={data.form} />
