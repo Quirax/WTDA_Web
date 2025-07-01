dfe4432c22cc0b94fe41747bed76e54cfae3b435
Quirax Lee <quiraxical@gmail.com>
2025-07-01 12:29:06 +0900

    리뷰: 불필요 코드 제거

--- src/stories/admin/Layout.svelte
+++ src/stories/admin/Layout.svelte
@@ -1,44 +1,8 @@
 <script lang="ts">
 	import SidebarLayout from '$stories/components/SidebarLayout.svelte';
 	import type { Snippet } from 'svelte';
-
-	import CalendarIcon from '@lucide/svelte/icons/calendar';
-	import HouseIcon from '@lucide/svelte/icons/house';
-	import InboxIcon from '@lucide/svelte/icons/inbox';
-	import SearchIcon from '@lucide/svelte/icons/search';
-	import SettingsIcon from '@lucide/svelte/icons/settings';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { browser } from '$app/environment';
 	import { page } from '$app/state';
-
-	// Menu items.
-	const items = [
-		{
-			title: 'Home',
-			url: '#',
-			icon: HouseIcon,
-		},
-		{
-			title: 'Inbox',
-			url: '#',
-			icon: InboxIcon,
-		},
-		{
-			title: 'Calendar',
-			url: '#',
-			icon: CalendarIcon,
-		},
-		{
-			title: 'Search',
-			url: '#',
-			icon: SearchIcon,
-		},
-		{
-			title: 'Settings',
-			url: '#',
-			icon: SettingsIcon,
-		},
-	];
 
 	let { children }: { children: Snippet } = $props();
 </script>
