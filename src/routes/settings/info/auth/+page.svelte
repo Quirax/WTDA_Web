220b7981e6e80bcf758b958640e6567c5c6f1365
Quirax Lee <quiraxical@gmail.com>
2025-07-01 11:20:23 +0900

    기타: `UserInfoAuth`를 다른 페이지에서도 사용할 수 있도록 일반화

--- src/routes/settings/info/auth/+page.svelte
+++ src/routes/settings/info/auth/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import UserInfoAuth from '$stories/settings/info/UserInfoAuth.svelte';
+	import PasswordAuth from '$stories/login/PasswordAuth.svelte';
 	import type { PageServerData } from './$types';
 
 	let { data }: { data: PageServerData } = $props();
 </script>
 
-<UserInfoAuth data={data.form} />
+<PasswordAuth data={data.form} />
