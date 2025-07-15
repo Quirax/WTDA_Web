e666fbc2f8ed6ea764997c889343cd9430366ac2
Quirax Lee <quiraxical@gmail.com>
2025-07-15 14:05:59 +0900

    메인 페이지에 paraglide 적용

--- src/stories/MainPage.svelte
+++ src/stories/MainPage.svelte
@@ -17,6 +17,7 @@
 	import Header from './components/Header.svelte';
 	import ArticleList from './components/ArticleList.svelte';
 	import { ArticleType } from '@app';
+	import { m } from '$lib/messages';
 
 	interface Props {
 		recentCommissionTypes?: App.CommisionType[];
@@ -24,28 +25,25 @@
 		introductions?: { src: string; alt: string }[];
 	}
 
-	const enum UserMode {
-		requester = 'requester',
-		commisioner = 'commisioner',
-	}
+	type UserMode = 'requester' | 'commissioner';
 
 	const { recentCommissionTypes = [], recentRequests = [], introductions = [] }: Props = $props();
 
-	let userMode = $state<UserMode>(UserMode.commisioner);
+	let userMode = $state<UserMode>('commissioner');
 </script>
 
-<Header title="뭐하지공방" />
+<Header title={m.SITE_NAME()} />
 
 <section
 	id="search"
 	class="bg-primary text-primary-foreground banner-pattern flex flex-col items-center justify-center space-y-10 p-20 pt-36">
-	<H1 class="text-center break-keep">뭐하지공방에 오신 것을 환영합니다</H1>
+	<H1 class="text-center break-keep">{m['MAIN_PAGE.WELCOME']()}</H1>
 
 	<!-- TODO 커미션 타입 관련 기능 추가 후 주석 해제
 	<Tabs.Root bind:value={userMode} class="md:w-[400px]">
 		<Tabs.List class="grid h-full! w-full md:grid-cols-2">
-			<Tabs.Trigger value="requester">저는 의뢰주입니다</Tabs.Trigger>
-			<Tabs.Trigger value="comissioner">저는 커미션주입니다</Tabs.Trigger>
+			<Tabs.Trigger value="requester">{m['MAIN_PAGE.TAB']({ userMode: 'requester' })}</Tabs.Trigger>
+			<Tabs.Trigger value="comissioner">{m['MAIN_PAGE.TAB']({ userMode: 'comissioner' })}</Tabs.Trigger>
 		</Tabs.List>
 	</Tabs.Root>
 	-->
@@ -61,42 +59,42 @@
 		<Input
 			type="search"
 			name="query"
-			placeholder={userMode === UserMode.requester ? '커미션 타입 찾기' : '의뢰 찾기'}
+			placeholder={m['MAIN_PAGE.SEARCH_PLACEHOLDER']({
+				articleType:
+					userMode === 'requester' ? m['ARTICLE_TYPE.COMMISSION']() : m['ARTICLE_TYPE.REQUEST'](),
+			})}
 			class="h-xl border-stone-200 bg-stone-50 text-xl text-stone-950 sm:w-full md:w-md" />
-		<Button type="submit" variant="secondary">검색</Button>
+		<Button type="submit" variant="secondary">{m['SEARCH.BUTTON']()}</Button>
 	</form>
 </section>
 
-{#if (userMode === UserMode.requester ? recentCommissionTypes : recentRequests).length > 0}
+{#if (userMode === 'requester' ? recentCommissionTypes : recentRequests).length > 0}
 	<Section class="pt-10" id="recently-added">
 		<H2 class="break-keep">
-			{#if userMode === UserMode.requester}
-				새로 개장한 커미션 타입들입니다
-			{:else}
-				새로 지원을 기다리는 의뢰들입니다
-			{/if}
+			{m['MAIN_PAGE.ARTICLE_LIST_TITLE']({ userMode: userMode })}
 		</H2>
 
 		<ArticleList
 			id="contents-list"
 			class="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
-			articles={userMode === UserMode.requester ? recentCommissionTypes : recentRequests}
-			moreLink="https://192.169.0.3:5173/search?query=&type={userMode === UserMode.requester
+			articles={userMode === 'requester' ? recentCommissionTypes : recentRequests}
+			moreLink="https://192.169.0.3:5173/search?query=&type={userMode === 'requester'
 				? 'COMMISSION'
 				: 'REQUEST'}" />
 	</Section>
 {/if}
 
 <!-- TODO 커미션 타입 관련 기능 추가 후 주석 해제
-{#if userMode === UserMode.requester}
+{#if userMode === 'requester'}
 	<section
 		id="suggestion"
 		class="bg-accent text-accent-foreground my-10 flex flex-col items-center justify-center space-y-8 p-10 text-center">
-		<H2 class="border-none break-keep">원하는 커미션 타입을 찾지 못하셨나요?</H2>
-		<Button class="p-6 text-xl">먼저 의뢰를 게시하세요!</Button>
+		<H2 class="border-none break-keep">{m['MAIN_PAGE.SUGGESTION.SUBTITLE']()}</H2>
+		<Button class="p-6 text-xl">{m['MAIN_PAGE.SUGGESTION.TITLE']()}</Button>
 		<div class="space-y-0">
-			<P>의뢰를 찾는 작가님들이 여러분의 의뢰에 먼저 지원할 수 있습니다.</P>
-			<P>물론 얼마든지 게시한 의뢰를 가지고 커미션을 직접 신청할 수도 있습니다.</P>
+			{#each m['MAIN_PAGE.SUGGESTION.DESCRIPTION']().split("\n") as desc}
+			<P>{desc}</P>
+			{/each}
 		</div>
 	</section>
 {/if}
