af154280a110eb65ef165f187f9bd1c482c64031
Quirax Lee <quiraxical@gmail.com>
2025-07-21 11:44:17 +0900

    데이터베이스 연결

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,39 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+import * as fs from 'fs';
 
+if (!process.env.DATABASE_URL) {
+	if (!process.env.DATABASE_HOST) throw new Error('DATABASE_HOST is not set');
+	if (!process.env.DATABASE_PORT) throw new Error('DATABASE_PORT is not set');
+	if (!process.env.DATABASE_USER) throw new Error('DATABASE_USER is not set');
+	if (!process.env.DATABASE_PASSWORD) throw new Error('DATABASE_PASSWORD is not set');
+	if (!process.env.DATABASE_SCHEMA) throw new Error('DATABASE_SCHEMA is not set');
+}
+
+// ref: https://stackoverflow.com/a/79285639/21742011
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		...(process.env.DATABASE_URL
+			? { url: process.env.DATABASE_URL }
+			: {
+					host: process.env.DATABASE_HOST!,
+					port: parseInt(process.env.DATABASE_PORT!),
+					user: process.env.DATABASE_USER!,
+					password: process.env.DATABASE_PASSWORD!,
+					database: process.env.DATABASE_SCHEMA!,
+				}),
+		...(process.env.DATABASE_CA_PATH
+			? {
+					ssl: {
+						checkServerIdentity: () => undefined,
+						ca: fs.readFileSync(process.env.DATABASE_CA_PATH),
+					},
+				}
+			: {}),
 	},
 
 	verbose: true,
 	strict: true,
-	dialect: 'postgresql'
+	dialect: 'postgresql',
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,40 @@ import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
 import { encodeBase32LowerCase } from '@oslojs/encoding';
 import { sql } from 'drizzle-orm';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = postgres(env.DATABASE_URL);
+import * as fs from 'fs/promises';
+import { join } from 'path';
+
+if (!env.DATABASE_URL) {
+	if (!env.DATABASE_HOST) throw new Error('DATABASE_HOST is not set');
+	if (!env.DATABASE_PORT) throw new Error('DATABASE_PORT is not set');
+	if (!env.DATABASE_USER) throw new Error('DATABASE_USER is not set');
+	if (!env.DATABASE_PASSWORD) throw new Error('DATABASE_PASSWORD is not set');
+	if (!env.DATABASE_SCHEMA) throw new Error('DATABASE_SCHEMA is not set');
+}
+
+// ref: https://stackoverflow.com/a/79285639/21742011
+const credentials = {
+	...(process.env.DATABASE_CA_PATH
+		? {
+				ssl: {
+					checkServerIdentity: () => undefined,
+					ca: await fs.readFile(join('.', process.env.DATABASE_CA_PATH)),
+				},
+			}
+		: {}),
+};
+
+const client = env.DATABASE_URL
+	? postgres(env.DATABASE_URL, credentials)
+	: postgres({
+			host: process.env.DATABASE_HOST!,
+			port: parseInt(process.env.DATABASE_PORT!),
+			user: process.env.DATABASE_USER!,
+			password: process.env.DATABASE_PASSWORD!,
+			database: process.env.DATABASE_SCHEMA!,
+			...credentials,
+		});
+
 export const db = drizzle(client);
 
 db.execute(sql`CREATE EXTENSION IF NOT EXISTS pg_trgm;`);
